9de3cef0dd498bd05ff850d9df19749ca79015b1
joseGS10 <josua75@hotmail.es>
2025-04-18 12:07:40 +0200

    Add files via upload

---
+++ TEMA3.JavaScript/ejercicio3/js03.js
@@ -0,0 +1,11 @@
+function cambiaCoche(nombre) {
+    document.getElementById('miscoches').src = nombre;   
+}
+
+function cambiaSpiderman(nombre) {
+    document.getElementById('misspiderman').src = nombre;
+}
+
+function cambiarEstilo(numeroestilo) {
+    document.getElementById('estilo').setAttribute("href", numeroestilo);
+}
